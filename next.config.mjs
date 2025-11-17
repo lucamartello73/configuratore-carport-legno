9687c6165df7b7201ba042e7bea89f264eaa2d96
lucamartello73 <209241945+lucamartello73@users.noreply.github.com>
2025-11-17 17:10:55 -0500

    fix: Aggiungi Unsplash ai domini permessi per le immagini

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,12 @@ const nextConfig = {
         hostname: 'placeholder.svg',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       }
     ],
     formats: ['image/webp', 'image/avif'],
